63861de2dbac0a1d70095aa81142f7ba2826ad91
Alex <lightskywrites@gmail.com>
2020-07-26 00:14:06 -0500

    The send mail button now sends the amount that is in the recorder.

--- L.lua
+++ L.lua
@@ -24,6 +24,7 @@ addonTbl.SetLocale = function(locale)
 		L["SLASH_CMD_1"]							= "/midas";
 		
 		-- ERROR MESSAGES
+		L["ERR_MSG_MAIL_CHARACTER_NOT_SET"]			= "No mail character set for this realm!";
 		L["ERR_MSG_MAP_NOT_SUPPORTED"]				= "Map not found. The map was temporarily added to the instances table so data can log. Please report the following map ID: ";
 		
 		-- GENERAL
--- Main.lua
+++ Main.lua
@@ -62,6 +62,19 @@ eventFrame:SetScript("OnEvent", function(self, event, ...)
 				if not MailFrameTab2.sendMailButton then -- Don't want the button to be created over and over
 					addonTbl.CreateWidget("Button", "sendMailButton", "", MailFrameTab2, "CENTER", MailFrameTab2, "RIGHT", 40, 400, 30, 30);
 					MailFrameTab2.sendMailButton:SetNormalTexture("Interface\\Minimap\\Tracking\\mailbox");
+					MailFrameTab2.sendMailButton:SetScript("OnClick", function(self)
+						if addonTbl[addonTbl.realmName].mailCharacter ~= nil then
+							local money = addonTbl.moneyObtainedThisSession;
+							local moneyLength = string.len(money);
+							SendMailNameEditBox:SetText(addonTbl[addonTbl.realmName].mailCharacter);
+							SendMailSubjectEditBox:SetText(addon);
+							SendMailMoney.copper:SetText(string.sub(money, moneyLength-1, moneyLength-0))
+							SendMailMoney.silver:SetText(string.sub(money, moneyLength-3, moneyLength-2))
+							SendMailMoney.gold:SetText(string.sub(money, 0, moneyLength-4))
+						else
+							print(L["ADDON_NAME"] .. L["ERR_MSG_MAIL_CHARACTER_NOT_SET"]);
+						end
+					end);
 					MailFrameTab2.sendMailButton:SetScript("OnEnter", function(self) addonTbl.ShowTooltip(self, L["SEND_MAIL_BUTTON"], nil) end);
 					MailFrameTab2.sendMailButton:SetScript("OnLeave", function(self) addonTbl.HideTooltip(self) end);
 				else
