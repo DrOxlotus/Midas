2d47e2a3b4cf47702100d32c40e5ae40835f7a3e
Alex <lightskywrites@gmail.com>
2020-08-02 01:30:19 -0500

    Another ignored map.

--- DB/DB_IgnoredMaps.lua
+++ DB/DB_IgnoredMaps.lua
@@ -14,6 +14,7 @@ local ignoredMaps = {
 	[1539]	= "Blackwing Descent",		-- BfA Legendary Cloak Scenario
 	[1540]	= "Halls of Origination",	-- BfA Legendary Cloak Scenario
 	[1541]	= "Halls of Origination",	-- BfA Legendary Cloak Scenario
+	[1542]	= "Halls of Origination",	-- BfA Legendary Cloak Scenario
 	[1544]	= "Mogu'Shan Palace",		-- BfA Legendary Cloak Scenario
 	[1545]	= "Mogu'Shan Palace",		-- BfA Legendary Cloak Scenario
 	[1549]	= "Mogu'Shan Palace",		-- BfA Legendary Cloak Scenario
