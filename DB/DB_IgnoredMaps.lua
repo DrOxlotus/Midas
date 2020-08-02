b06184b48d450ca1ed6a04c30c7ea4d42fe46867
Alex <lightskywrites@gmail.com>
2020-08-02 00:36:24 -0500

    Ignored more maps.

--- DB/DB_IgnoredMaps.lua
+++ DB/DB_IgnoredMaps.lua
@@ -10,6 +10,9 @@ local ignoredMaps = {
 	[718]	= "Dreadscar Rift",			-- Warlock Class Hall Unlock
 	[773]	= "Tol Barad",				-- Destruction Warlock Artifact
 	[774]	= "Baradin Hold",			-- Destruction Warlock Artifact
+	[1544]	= "Mogu'Shan Palace",		-- BfA Legendary Cloak Scenario
+	[1545]	= "Mogu'Shan Palace",		-- BfA Legendary Cloak Scenario
+	[1549]	= "Mogu'Shan Palace",		-- BfA Legendary Cloak Scenario
 };
 
 addonTbl.ignoredMaps = ignoredMaps;
